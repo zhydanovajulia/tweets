ac21888a1bb678ffd4edf5e8ac3452a13052a959
Julia Zhydanova <jz@anahoret.com>
2013-06-04 12:41:55 +0300

    Install twitter gem.

---
+++ config/initializers/twitter.rb
@@ -0,0 +1,6 @@
+Twitter.configure do |config|
+  config.consumer_key = "D1KiJJ9govlq1bnNNVfUlw"
+  config.consumer_secret = "AvwwR0I5sUomGd9F5sdfyxAzzajTnNXNyevy0KvSw"
+  config.oauth_token = "254776905-2hL1w4oe2uY5DN8L6fGC8yW2itoIYo60TdMGFJYn"
+  config.oauth_token_secret = "wNaj9zL7RcLHJqUJGVjSzQJEKio9DZB6iLbklCvh9U"
+end
